3fac01ee95b4409425edc536eb8305bac46223b1
Tommo4617 <AbbieT0406@gmail.com>
2025-07-27 01:18:28 +0100

    Main login menu created
    Main login allows for options to be displayed to the user, and one option (sign up) has some functionality

---
+++ Period tracker 1/Period tracker 1/Startup Menu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Period_tracker_1
+{
+    internal class Startup_Menu : MenuMaker
+    {
+
+        public Startup_Menu() : base("Welcome to the period tracker!", ["Login", "Sign Up"]){
+
+        
+
+        }
+
+        public override void ChoiceAction(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Login selected.");
+                    // Add login logic here
+                    break;
+                case 2:
+                    Console.WriteLine("Sign Up selected.");
+                    SignUp();
+                    // Add sign-up logic here
+                    break;
+                case 3:
+                    Console.WriteLine("Exiting the application.");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+
+        private void SignUp()
+        {
+            bool validUsername = false;
+            bool validPassword = false;
+
+            while (!validUsername)
+            {
+                Console.WriteLine("Please enter your username:");
+                string username = Console.ReadLine();
+                if (!string.IsNullOrEmpty(username))
+                {
+                    validUsername = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Username is invalid, please try again.");
+                }
+            }
+
+            while (!validPassword)
+            {
+                Console.WriteLine("Please enter your password:");
+                string password = Console.ReadLine();
+                Console.WriteLine("Please confirm your password:");
+                string passwordConfirm = Console.ReadLine();
+
+                if(!string.IsNullOrEmpty(password) && password == passwordConfirm)
+                {
+                    validPassword = true;
+                    Console.WriteLine("Password confirmed, navigating back to the main menu.");
+
+                    for(int i = 0; i < 3; i++)
+                    {
+                        Console.Write(".");
+                        Thread.Sleep(500);
+                    }
+                    Startup_Menu startup_Menu = new Startup_Menu();
+                    startup_Menu.DisplayMenu();
+                    startup_Menu.ChoiceAction(int.Parse(Console.ReadLine()));
+
+                }
+                else
+                {
+                    Console.WriteLine("Password is invalid or does not match, please try again.");
+                }
+            }
+            
+
+
+
+        }
+    }
+}
